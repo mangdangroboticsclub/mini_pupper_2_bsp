a3b7c35a5684c74b32ab0bb42285004ce4cc9eb1
Horst Dumcke <hdumcke@marlysys.com>
2023-07-19 12:26:40 +0200

    fixed is-release test and fixed spelling mistake of demo script name

--- install.sh
+++ install.sh
@@ -19,8 +19,8 @@ echo BSP_VERSION=\"$(cd ~/mini_pupper_bsp; ./get-version.sh)\" >> ~/mini-pupper-
 cd ~/mini_pupper_bsp
 TAG_COMMIT=$(git rev-list --abbrev-commit --tags --max-count=1)
 TAG=$(git describe --abbrev=0 --tags ${TAG_COMMIT} 2>/dev/null || true)
-BSP_VERSION=./get-version.sh
-if [ "v$BSP_VERSION" == "TAG" ]
+BSP_VERSION=$(./get-version.sh)
+if [ "v$BSP_VERSION" == "$TAG" ]
 then
     echo IS_RELEASE=YES >> ~/mini-pupper-release
 else
